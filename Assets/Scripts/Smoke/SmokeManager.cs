27aa7649382c5e4eda6c41f9c58241579df83501
MrEsport <lucasespositonc@gmail.com>
2025-08-01 10:48:18 +0200

    * Targets to validate and regenerate

--- Assets/Scripts/Smoke/SmokeManager.cs
+++ Assets/Scripts/Smoke/SmokeManager.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+using Random = UnityEngine.Random;
 
 public class SmokeManager : MonoBehaviour
 {
     private static SmokeManager instance;
     public static SmokeManager Instance { get => instance; }
 
-    private List<GameObject> smokeParticles = new();
+    private List<SmokePoint> smokeParticles = new();
+    [SerializeField] private List<Vector2> targetsPositions = new();
+    private List<SmokePointTarget> smokePointTargets = new();
 
     private void Awake()
     {
@@ -19,17 +25,141 @@ public class SmokeManager : MonoBehaviour
         instance = this;
     }
 
+    private void Start()
+    {
+        GeneratePointTargets();
+    }
+
+    private void Update()
+    {
+        if (smokeParticles.Count <= 1) return;
+
+        foreach (var point in smokeParticles)
+        {
+            if (!smokePointTargets.Any(t => !t.isValid))
+            {
+                // ALL TARGETS VALID : RESET TARGETS
+                ResetTargets();
+                break;
+            }
+
+            var closePoints = smokePointTargets.Where(t => !t.isValid && Vector2.SqrMagnitude(t.position - point.position) <= .25f * .25f);
+            if (closePoints.Count() <= 0) continue;
+
+            closePoints.First().SetValid(point);
+        }
+
+        foreach (var target in smokePointTargets.Where(t => t.isValid))
+        {
+            DebugExtension.DrawCircle(target.validatingPoint.position, .25f, 10, ColorExtension.red);
+            Debug.DrawLine(target.validatingPoint.position, target.position, ColorExtension.darkRed);
+        }
+    }
+
     public void OnClearSmokeInputReceived(InputAction.CallbackContext context)
     {
         if (!context.started) return;
-        foreach (var particle in smokeParticles)
-            Destroy(particle.gameObject);
-
-        smokeParticles.Clear();
+        ClearSmoke();
     }
 
     public void RegisterSmoke(GameObject particle)
     {
-        smokeParticles.Add(particle);
+        var point = new SmokePoint(particle, () => DestroyCallback(particle));
+        smokeParticles.Add(point);
+    }
+
+    private void DestroyCallback(GameObject obj)
+    {
+        Destroy(obj);
+    }
+
+    private void ClearSmoke()
+    {
+        foreach (var smoke in smokeParticles)
+            smoke.Dispose();
+
+        smokeParticles.Clear();
+    }
+
+    private void GeneratePointTargets()
+    {
+        targetsPositions.Clear();
+
+        int N = Random.Range(4, 8);
+
+        for (; N > 0; --N)
+        {
+            targetsPositions.Add(RandomExtension.PointInsideBox(Vector2.zero, 10f));
+        }
+
+        smokePointTargets = targetsPositions.Select(p => new SmokePointTarget(p)).ToList();
+    }
+
+    private void ResetTargets()
+    {
+        ClearSmoke();
+        GeneratePointTargets();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (smokePointTargets.Count <= 0) return;
+        Color pastColor = Gizmos.color;
+        foreach (var target in smokePointTargets)
+        {
+            Gizmos.color = target.isValid ? Color.green : ColorExtension.orange;
+            Gizmos.DrawWireSphere(target.position, .25f);
+        }
+        Gizmos.color = pastColor;
+    }
+
+    private void OnGUI()
+    {
+        int validTargets = smokePointTargets.Count(t => t.isValid);
+        GUI.TextField(new Rect(0, 0, 500, 25), $"Targets : {validTargets} / {smokePointTargets.Count()}");
+
+        if (GUI.Button(new Rect(Screen.width - 200, 0, 200, 25), "RESET TARGETS"))
+            ResetTargets();
+        if (GUI.Button(new Rect(Screen.width - 200, 28, 200, 25), "CLEAR SMOKE"))
+            ClearSmoke();
+    }
+}
+
+public class SmokePoint : IDisposable
+{
+    public Vector2 position { get; private set; }
+    
+    private Action destroyCallback;
+
+    public SmokePoint(GameObject particle, Action destroyCallback)
+    {
+        position = particle.transform.position;
+        this.destroyCallback = destroyCallback;
+    }
+
+    public void Dispose()
+    {
+        if (destroyCallback == null) return;
+        destroyCallback();
+    }
+}
+
+public class SmokePointTarget
+{
+    public Vector2 position { get; private set; }
+    public bool isValid { get; private set; }
+
+    public SmokePoint validatingPoint { get; private set; }
+
+    public SmokePointTarget(Vector2 position)
+    {
+        this.position = position;
+        isValid = false;
+    }
+
+    public void SetValid(SmokePoint point)
+    {
+        validatingPoint = point;
+        isValid = true;
     }
 }
